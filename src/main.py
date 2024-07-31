c468b2eac86fd6a2b137af655e78b965c646cf1e
Vendron <gjc@clews.biz>
2024-07-31 02:00:04 +0100

    + Remove complications + Refactor code into classes and methods + Simply the algorithm using skorch NeuralNetBinaryClassifier over NeuralNetClassifier + ADD: Logger for additional debugging + ADD: clearer documentation. + ADD: Set hyperparams as constants for easier access + ADD: torch and numpy seed to assist with confusion matrix problem + REMOVE: redundant directories and files

--- src/main.py
+++ src/main.py
@@ -1,92 +1,208 @@
-from typing import List, Dict
-import pandas as pd
-import numpy as np
-from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score
-from sklearn.model_selection import KFold, cross_validate
-from sklearn.preprocessing import MinMaxScaler
-from scikit_longitudinal.data_preparation import LongitudinalDataset
-from models.mlp.mlp import TemporalMLP
-import os
+from typing import List
 from dotenv import load_dotenv
+import os
+from scikit_longitudinal.data_preparation import LongitudinalDataset
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.nn import Module, Linear, Sigmoid, Sequential, BCEWithLogitsLoss, ModuleList
+from torch import Tensor, cat
+from torch.nn.functional import dropout
+from skorch import NeuralNetBinaryClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score
+from sklearn.preprocessing import MinMaxScaler
+import logging
 
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get variables from environment
+# Constants
 DATASET_PATH: str = os.getenv("DATASET_PATH")
 TARGET_WAVE: str = os.getenv("TARGET_WAVE")
+# Hyperparameters
+INPUT_SIZE: int = None  # Will be set after loading data 
+HIDDEN_SIZE: int = 64 # Number of neurons in each hidden layer
+OUTPUT_SIZE: int = 1 # Number of neurons in the output layer
+DROPOUT_RATE: float = 0.2 # Dropout rate for regularization (Dropout layer is applied after hidden layers to prevent overfitting by randomly setting a fraction of input units to 0)
+# Specific hyperparameters for the MLP model
+MAX_EPOCHS: int = 100 # Maximum number of epochs for training
+LR: float = 0.001 # Learning rate for optimization (Adam optimizer is used by default, however other optimizers can be used by specifying the optimizer parameter in the NeuralNetBinaryClassifier constructor)
+ITERATOR_TRAIN_SHUFFLE: bool = True # Shuffle the training data before each epoch
+TRAIN_SPLIT: None = None # Use the entire training data for training
+VERBOSE: int = 1 # Verbosity level for logging during training, where 0 = silent, 1 = progress bar, 2 = one line per epoch and 3 = one line per batch.
+CRITERION: nn.Module = BCEWithLogitsLoss() # Binary Cross-Entropy loss function for binary classification tasks. This function combines a Sigmoid layer and the BCE loss in one single class.
 
-dataset: LongitudinalDataset = LongitudinalDataset(DATASET_PATH)
-dataset.load_data_target_train_test_split(target_column=TARGET_WAVE, random_state=42)
-dataset.setup_features_group("elsa")
-
-X_train, X_test, y_train, y_test = dataset.X_train, dataset.X_test, dataset.y_train, dataset.y_test
+# Set seeds for reproducibility - Note later: Issue with re-recurring confusion matrix values.  
+torch.manual_seed(42)
+np.random.seed(42)
 
 def preprocess_data(X: np.ndarray) -> np.ndarray:
     """
-    Preprocesses the input data array by replacing '?' with NaN.
+    Preprocess the input data by handling missing values and converting all entries to numeric types.
+    
+    This function replaces missing values (denoted by '?') with NaN, coerces all data to numeric types,
+    and fills NaN values with 0. The preprocessed data is returned as a NumPy array.
 
     Args:
-        X (np.ndarray): The input data array.
+        X (np.ndarray): The input data to be preprocessed.
 
     Returns:
-        np.ndarray: The preprocessed data as a NumPy array.
+        np.ndarray: The preprocessed data.
     """
     X_df: pd.DataFrame = pd.DataFrame(X)
     X_df.replace('?', np.nan, inplace=True)
+    X_df = X_df.apply(pd.to_numeric, errors='coerce')
     X_df.fillna(0, inplace=True)
     return X_df.values
 
-X_train: np.ndarray = preprocess_data(X_train)
-X_test: np.ndarray = preprocess_data(X_test)
+def load_and_preprocess_data() -> tuple:
+    """
+    Load and preprocess the longitudinal dataset.
+    
+    This function loads the dataset, splits it into training and test sets, preprocesses the features,
+    and normalizes the data using Min-Max scaling. It also sets the global INPUT_SIZE variable and
+    returns the preprocessed training and test sets, target values, and feature groups.
 
-# Normalize data
-scaler: MinMaxScaler = MinMaxScaler()
-X_train: np.ndarray = scaler.fit_transform(X_train).astype(np.float32)
-X_test: np.ndarray = scaler.transform(X_test).astype(np.float32)
+    Returns:
+        tuple: A tuple containing the preprocessed training data, test data, training targets, test targets, and feature groups.
+    """
+    dataset: LongitudinalDataset = LongitudinalDataset(DATASET_PATH)
+    dataset.load_data_target_train_test_split(target_column=TARGET_WAVE, random_state=42)
+    dataset.setup_features_group("elsa")
 
-# Convert y to numeric values (float32) because skorch requires float32
-y_train: np.ndarray = pd.to_numeric(y_train, errors='coerce').values.astype(np.float32)
-y_test: np.ndarray = pd.to_numeric(y_test, errors='coerce').values.astype(np.float32)
+    X_train: np.ndarray = preprocess_data(dataset.X_train)
+    X_test: np.ndarray = preprocess_data(dataset.X_test)
+    y_train: np.ndarray = pd.to_numeric(dataset.y_train, errors='coerce').values
+    y_test: np.ndarray = pd.to_numeric(dataset.y_test, errors='coerce').values
 
-# Model parameters
-input_size: int = X_train.shape[1]
-hidden_sizes: List[int] = [64, 32]  # List of hidden layer sizes
-output_size: int = 1
-epochs: int = 250
-learning_rate: float = 0.01
-dropout_rate: float = 0.5
-features_group: List[List[int]] = dataset.feature_groups()
+    # Normalize data
+    scaler: MinMaxScaler = MinMaxScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
-# Initialize and train the model
-mlp: TemporalMLP = TemporalMLP(input_size, hidden_sizes, output_size, dropout_rate, epochs, learning_rate, features_group)
+    global INPUT_SIZE
+    INPUT_SIZE = X_train.shape[1]
 
-# Reshape y_train to 2D array
-y_train: np.ndarray = y_train.reshape(-1, 1)
+    return X_train, X_test, y_train, y_test, dataset.feature_groups()
 
-kf: KFold = KFold(n_splits=5, shuffle=True, random_state=42)
-cv_results: Dict[str, List[float]] = cross_validate(mlp, X_train, y_train, cv=kf, scoring=['accuracy', 'precision', 'recall', 'f1', 'roc_auc'])
+class LongitudinalMLPModule(Module):
+    def __init__(self, hidden_size: int, output_size: int, dropout_rate: float, features_group: List[List[int]]) -> None:
+        """
+        Initialize the Multi-Layer Perceptron (MLP) model designed for longitudinal data.
+        
+        This constructor initializes the model with hidden layers for each feature group and 
+        sets the dropout rate for regularization. Each feature group is processed by a separate 
+        hidden layer, and their outputs are concatenated before being passed to the final output layer.
 
-print(f'CV Results: {cv_results}')
-print(f'Accuracy: {np.mean(cv_results["test_accuracy"])}')
+        Args:
+            hidden_size (int): The number of neurons in each hidden layer.
+            output_size (int): The number of neurons in the output layer.
+            dropout_rate (float): The dropout rate used for regularization.
+            features_group (List[List[int]]): A list of feature groups, where each group contains indices of features.
+        """
+        super(LongitudinalMLPModule, self).__init__()
+        self.features_group: List[List[int]] = features_group
+        self.hidden_layers: ModuleList = ModuleList()
+        self.dropout_rate: float = dropout_rate
 
-mlp.fit(X_train, y_train)
+        for group in features_group:
+            group_size: int = len(group)
+            self.hidden_layers.append(Sequential(
+                Linear(group_size, hidden_size),
+                Sigmoid()
+            ))
 
-# Predict and evaluate
-y_pred: np.ndarray = mlp.predict(X_test)
-accuracy = np.mean(y_pred == y_test.reshape(-1, 1))
+        self.output_layer: Linear = Linear(hidden_size * len(features_group), output_size)
 
-# Calculate additional metrics
-precision: float = precision_score(y_test, y_pred)
-recall: float = recall_score(y_test, y_pred)
-f1: float = f1_score(y_test, y_pred)
-conf_matrix: np.ndarray = confusion_matrix(y_test, y_pred)
-roc_auc: float = roc_auc_score(y_test, y_pred)
+    def forward(self, X: Tensor) -> Tensor:
+        """
+        Forward pass through the MLP model.
+        
+        The input tensor is split into groups of features, which are then processed separately by
+        corresponding hidden layers. The outputs of these layers are concatenated and passed through
+        a dropout layer before being passed to the final output layer to generate the model's predictions.
 
-print(f'Accuracy: {accuracy}')
-print(f'Precision: {precision}')
-print(f'Recall: {recall}')
-print(f'F1 Score: {f1}')
-print(f'Confusion Matrix:\n{conf_matrix}')
-print(f'ROC-AUC Score: {roc_auc}')
+        Args:
+            X (torch.Tensor): The input tensor.
+
+        Returns:
+            torch.Tensor: The output tensor.
+        """
+        group_outputs: List[Tensor] = []
+
+        for idx, group in enumerate(self.features_group):
+            group_x: Tensor = X[:, group]
+            group_output: Tensor = self.hidden_layers[idx](group_x)
+            group_outputs.append(group_output)
+
+        concatenated_outputs: Tensor = cat(group_outputs, dim=1)
+        concatenated_outputs = dropout(concatenated_outputs, p=self.dropout_rate, training=self.training)
+        output: Tensor = self.output_layer(concatenated_outputs)
+
+        return output
+
+def train_and_evaluate_model(X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray, features_group: List[List[int]]) -> None:
+    """
+    Train the MLP model and evaluate its performance on the test data.
+    
+    This function initializes the model with the specified hyperparameters, trains it on the training
+    data, and evaluates its performance on the test data by calculating various metrics such as 
+    accuracy, precision, recall, F1 score, confusion matrix, and ROC-AUC score. The results are logged
+    for further analysis.
+
+    Args:
+        X_train (np.ndarray): The preprocessed training data.
+        X_test (np.ndarray): The preprocessed test data.
+        y_train (np.ndarray): The training target values.
+        y_test (np.ndarray): The test target values.
+        features_group (List[List[int]]): A list of feature groups, where each group contains indices of features.
+    """
+    model: NeuralNetBinaryClassifier = NeuralNetBinaryClassifier(
+        LongitudinalMLPModule(HIDDEN_SIZE, OUTPUT_SIZE, DROPOUT_RATE, features_group),
+        max_epochs=MAX_EPOCHS,
+        lr=LR,
+        iterator_train__shuffle=ITERATOR_TRAIN_SHUFFLE,
+        train_split=TRAIN_SPLIT,
+        verbose=VERBOSE,
+        criterion=CRITERION
+    )
+
+    logger.info("Starting model training...")
+    model.fit(X_train.astype(np.float32), y_train.astype(np.float32))
+
+    logger.info("Evaluating model...")
+    y_predictions: np.ndarray = model.predict(X_test.astype(np.float32))
+    accuracy: float = np.mean(y_predictions == y_test)
+    logger.info(f'Accuracy: {accuracy:.4f}')
+
+    # Calculate additional metrics
+    precision: float = precision_score(y_test, y_predictions)
+    recall: float = recall_score(y_test, y_predictions)
+    f1: float = f1_score(y_test, y_predictions)
+    conf_matrix: np.ndarray = confusion_matrix(y_test, y_predictions)
+    roc_auc: float = roc_auc_score(y_test, y_predictions)
+
+    logger.info(f'Precision: {precision:.4f}')
+    logger.info(f'Recall: {recall:.4f}')
+    logger.info(f'F1 Score: {f1:.4f}')
+    logger.info(f'Confusion Matrix:\n{conf_matrix}')
+    logger.info(f'ROC-AUC Score: {roc_auc:.4f}')
+    logger.info(f"Model target: {TARGET_WAVE}")
+
+def main() -> None:
+    """
+    The main function orchestrates the loading, preprocessing, training, and evaluation processes.
+    
+    This function serves as the entry point of the script, calling functions to load and preprocess
+    the data, train the MLP model, and evaluate its performance.
+    """
+    X_train, X_test, y_train, y_test, features_group = load_and_preprocess_data()
+    train_and_evaluate_model(X_train, X_test, y_train, y_test, features_group)
+
+if __name__ == "__main__":
+    main()
